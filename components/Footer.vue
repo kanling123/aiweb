2870a4936fe401d9dd2c2e45e6ed89ad6302017a
unknown <1806186849@qq.com>
2026-08-18 20:21:28 +0800

    aiweb

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {version} from '../package.json'
+
+const v = ref(version)
+</script>
+
+<template>
+  <div class="mt-auto shrink-0 h-8">
+    <div class="text-center text-xs font-light">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
